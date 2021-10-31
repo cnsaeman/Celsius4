d583bb0828841281003d51eead20b6edc2b7f6f7
cnsaeman <csaemann@gmail.com>
2021-10-31 09:55:36 +0000

    second source commit

--- plugins-old/test.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class test
-{
-	public static void main(String[] args) {
-		String test="@Article{Klauder:2018:1-15,\n   journal = \"Springer Proceedings in Physics\",\n   volume = \"\",\n   journal = \"Springer Proceedings in Physics\",\n }";
-		System.out.println(test.replaceAll("\\s+volume\\s+\\= \\\"\\\",",""));
-	}
-}
